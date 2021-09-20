ad7e7862550bcdb54af81c26ff3e971decfdd70a
geovani <geovany.tj@gmail.com>
2021-09-20 16:26:41 -0300

    correção

--- src/pages/ncm.jsx
+++ src/pages/ncm.jsx
@@ -1,5 +1,5 @@
 import Button from "../components/button/button"
-import Header from "../components/Header/Header"
+import Header from "../components/Header/header"
 import Input from "../components/input/input"
 
 export default function NCM() {
